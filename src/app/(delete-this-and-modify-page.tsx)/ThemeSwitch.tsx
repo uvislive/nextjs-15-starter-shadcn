84fdaaa6632f39d78d2082574bc6b0ed9d7dcb0a
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-10-29 21:28:15 +0530

    refactor: Examples removed

--- src/app/(delete-this-and-modify-page.tsx)/ThemeSwitch.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/ThemeSwitch.tsx
@@ -22,12 +22,12 @@ const SWITCH_DATA: SwitchOption[] = [
     {
         name: 'Light',
         value: 'light',
-        iconSvg: (<svg xmlns="http://www.w3.org/2000/svg" className="size-5" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0-8 0m-5 0h1m8-9v1m8 8h1m-9 8v1M5.6 5.6l.7.7m12.1-.7l-.7.7m0 11.4l.7.7m-12.1-.7l-.7.7"></path></svg>),
+        iconSvg: (<svg xmlns="http://www.w3.org/2000/svg" className="size-5" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokelinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0-8 0m-5 0h1m8-9v1m8 8h1m-9 8v1M5.6 5.6l.7.7m12.1-.7l-.7.7m0 11.4l.7.7m-12.1-.7l-.7.7"></path></svg>),
     },
     {
         name: 'Dark',
         value: 'dark',
-        iconSvg: (<svg xmlns="http://www.w3.org/2000/svg" className="size-4" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z"></path></svg>),
+        iconSvg: (<svg xmlns="http://www.w3.org/2000/svg" className="size-4" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokelinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3h.393a7.5 7.5 0 0 0 7.92 12.446A9 9 0 1 1 12 2.992z"></path></svg>),
     },
 ];
 
